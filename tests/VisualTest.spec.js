59eda748597d6642f1aa41a5d436086c7548a240
abhiknag32 <69314596+abhiknag32@users.noreply.github.com>
2026-07-11 18:08:13 +0530

    Change

--- tests/VisualTest.spec.js
+++ tests/VisualTest.spec.js
@@ -4,5 +4,5 @@ test("Visual Test", async({page})=> {
 
     await page.goto("https://dic.gov.in/careers/?search_keywords=&selected_category=-1&selected_jobtype=-1&selected_location=noida");
     expect(await page.screenshot()).toMatchSnapshot('landing.png');
-
+    console.log("Visual test completed");
 })
