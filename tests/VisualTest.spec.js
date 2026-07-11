c99c72ca11d2d2f29b7c28f911d51b756bb16e13
Ruman <guhaniyogi.ruman@gmail.com>
2026-07-11 19:22:04 +0530

    Rumans 4th merge conflict

--- tests/VisualTest.spec.js
+++ tests/VisualTest.spec.js
@@ -16,4 +16,6 @@ test("Visual Test", async({page})=> {
     console.log("Ruman Change 2 applied");
     // Ruman Change 3
     console.log("Ruman Change 3 applied");
+    // Ruman Change 4
+    console.log("Ruman Change 4 applied");
 })
