077f0a6437823d868c1e44ec73d3629047bc7259
Ruman <guhaniyogi.ruman@gmail.com>
2026-07-11 18:16:21 +0530

    Ruman's Changes

--- tests/VisualTest.spec.js
+++ tests/VisualTest.spec.js
@@ -2,7 +2,7 @@ const{test,expect} = require('@playwright/test')
 
 test("Visual Test", async({page})=> {
 
-    await page.goto("https://dic.gov.in/careers/?search_keywords=&selected_category=-1&selected_jobtype=-1&selected_location=noida");
+    await page.goto("https://www.google.com");
     expect(await page.screenshot()).toMatchSnapshot('landing.png');
 
 })
